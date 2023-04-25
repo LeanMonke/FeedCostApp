58d62fb5ee207d7e2175664db60a2681e1b68029
Lean Monke <99847359+LeanMonke@users.noreply.github.com>
2023-04-26 09:10:07 +1200

    Add project files.

---
+++ FeedCostApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FeedCostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
