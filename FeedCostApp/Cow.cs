3ad4043e3070bd0c7e04db37856336b35f15c690
Lean Monke <99847359+LeanMonke@users.noreply.github.com>
2023-06-22 12:27:49 +1200

    lots of progress

---
+++ FeedCostApp/Cow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeedCostApp
+{
+    class Cow
+    {
+        //cow atributes
+        private string breed;
+        private DateTime dob;
+        private string id;
+        private string foodType;
+        private List<float> dailyFoodConsumed;
+
+        //constructor
+        public Cow(string breed, DateTime dob,string foodType)
+        {
+            this.breed = breed;
+            this.dob = dob;
+            this.foodType = foodType;
+        }
+
+        //adds daily consumption to a daily food consumed list
+        public void AddConsumption(float consumption)
+        {
+            dailyFoodConsumed.Add(consumption);
+        }
+        //calculates the total food consumed in a week 
+        public float CalculateFoodConsumption()
+        {
+            float totalFoodAmount = 0;
+
+            if (dailyFoodConsumed.Count < 7)
+            {
+                for (int index = 0; index < dailyFoodConsumed.Count; index++)
+                            {
+                    totalFoodAmount += dailyFoodConsumed[index];
+                            }
+            }
+            else
+            {
+                for (int index = dailyFoodConsumed.Count -7; index < dailyFoodConsumed.Count; index++)
+                {
+                    totalFoodAmount += dailyFoodConsumed[index];
+                }
+            }
+            
+            return totalFoodAmount;
+        }
+        //calculates the total cost for a week of feed
+        public float CalculateFoodCost(float foodCost)
+        {
+            return CalculateFoodConsumption()*foodCost;
+        }
+    }
+}
---
+++ FeedCostApp/CowManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeedCostApp
+{
+    class CowManager
+    {
+        private List<Cow> cows;
+        private List<string> breeds;
+        private List<string> foodTypes;
+        private List<float> foodCosts;
+
+        public CowManager()
+        {
+            this.cows = new List<Cow>();
+            this.breeds = new List<string>()
+            {
+                "Freisan"
+            };
+            this.foodTypes = new List<string>()
+            {
+                "silage"
+            };
+            this.foodCosts = new List<float>()
+            {
+                0f
+            };
+        }
+        public float CalculateBreedFeedCost()
+        {
+            return 0;
+        }
+    }
+}
